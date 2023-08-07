9e3ffa46630e5895c41b90ee050afd02a70aac9f
tolucde <toluphilip619@gmail.com>
2023-08-07 23:25:26 +0100

    setting of context and reducers, onboarding and login page created

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react'
+import Image from 'next/image'
+import { useStateProvider } from '@/context/StateContext'
 
 function onboarding() {
-  return <div>onboarding</div>;
+  const [{ userInfo }] = useStateProvider()
+
+  return (
+    <div className='bg-panel-header-background h-screen w-screen text-white flex flex-col item'>
+      <div className='flex items-center justify-center gap-2'>
+        <Image src='/whatsapp.gif' alt='whatsapp' height={300} width={300} />
+        <span className='text-7xl'>Whatsapp</span>
+      </div>
+      <h2 className='text-2xl'>Create your profile</h2>
+      <div className='flex gap-6 mt-6'>
+        <div className='flex flex-col items-center justify-center mt-5 gap-6'>
+          {userInfo?.name}
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default onboarding;
+export default onboarding
